44e1b1399f9205a6269813a818bf983e302037ae
unknown <an256.ak@gmail.com>
2020-04-01 22:28:19 +0300

    feat: create footer

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@ var menuButton = document.querySelector('.menu__btn');
 var menu = document.querySelector('.menu');
 menuButton.addEventListener('click', function () {
   menuButton.classList.toggle('menu__btn--active');
-  console.log('кликоткрыть меню');
   menu.classList.toggle('menu--active');  
 })
 
